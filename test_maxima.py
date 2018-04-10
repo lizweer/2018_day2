56be7cb85e00edfa3e7527eb417d474b7eee2f2a
Liz Weerdmeester <weerdmeester.liz@gmail.com>
2018-04-10 10:49:39 +0200

    last version before merge

--- test_maxima.py
+++ test_maxima.py
@@ -1,18 +1,26 @@
 from maxima import *
 import numpy as np
 
-def test_find_maxima():
-    works = True
+def test_find_maxima_1():
     x = [0, 1, 2, 1, 2, 1, 0]
-    if find_maxima(x) != [2,4]:
-        works = False
+    assert find_maxima(x) == [2,4]
 
+def test_find_maxima_2():
     x = [-i**2 for i in range(-3, 4)]
-    if find_maxima(x) != [3]:
-        works = False
+    assert find_maxima(x) == [3]
 
+def test_find_maxima_3():
     x = [np.sin(2*alpha) for alpha in np.linspace(0.0, 5.0, 100)]
-    if find_maxima(x) != [16,78]:
-        works = False
+    assert find_maxima(x) == [16,78]
 
-    return works
+def test_find_maxima_4():
+    x = [4, 2, 1, 3, 1, 2]
+    assert find_maxima(x) == [0,3,5]
+
+def test_find_maxima_5():
+    x = [4, 2, 1, 3, 1, 5]
+    assert find_maxima(x) == [0,3,5]
+
+def test_find_maxima_6():
+    x = [4, 2, 1, 3, 1]
+    assert find_maxima(x) == [0,3]
